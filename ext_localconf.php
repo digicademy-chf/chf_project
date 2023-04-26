78e7b5d9905d091393787912a299773bffa39148
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2023-04-26 21:56:53 +0200

    Add general backend customisations

--- ext_localconf.php
+++ ext_localconf.php
@@ -28,4 +28,19 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 // Prevent script from being called directly
 defined( 'TYPO3' ) or die();
 
+// Register content for this extension
+( function( $extKey='namenforschung' ) {
+
+    // Backend customisation
+    $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend'] = [
+        'backendFavicon'       => 'EXT:' . $extKey . '/Resources/Public/Images/favicon-small.svg',
+        'backendLogo'          => 'EXT:' . $extKey . '/Resources/Public/Icons/Backend.svg',
+        'loginBackgroundImage' => 'EXT:' . $extKey . '/Resources/Public/Icons/Background.svg',
+        'loginLogo'            => 'EXT:' . $extKey . '/Resources/Public/Icons/Login.svg',
+        'loginLogoAlt'         => 'Portal für Namenforschung',
+        'loginHighlightColor'  => '#5ac918'
+    ];
+
+} )();
+
 ?>
